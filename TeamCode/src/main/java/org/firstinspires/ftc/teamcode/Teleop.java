10fad53694ef422e4386755f69d537a133595f87
NotAnt0n <amashkin06@gmail.com>
2023-11-16 12:15:25 -0500

    Drive constant added. IMU orientation. Simple Tele

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -0,0 +1,120 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.Util;
+import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
+
+
+@TeleOp(name = "Teleop ArmV1", group = "TeleOp")
+public class Teleop extends LinearOpMode {
+    //Claw
+    Servo claw1;
+    Servo claw2;
+
+
+
+    //Drive
+    SampleMecanumDrive mecanumDrive;
+
+    //Arm
+    DcMotorEx armMotor1;
+    DcMotorEx armMotor2;
+
+    Gamepad currentGamepad2 = new Gamepad();
+    Gamepad previousGamepad2 = new Gamepad();
+    private double driveValue = 1;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //Drive
+        mecanumDrive = new SampleMecanumDrive(hardwareMap);
+
+        //Claw
+        claw1 = hardwareMap.get(Servo.class, "claw1");
+        claw2 = hardwareMap.get(Servo.class, "claw2");
+
+        //Arm
+        armMotor1 = hardwareMap.get(DcMotorEx.class, "arm1");
+        armMotor2 = hardwareMap.get(DcMotorEx.class, "arm2");
+
+
+        waitForStart();
+        //Setting ZeroPowerBehavior, should stop motor from rotating when not powered.
+        armMotor1.setZeroPowerBehavior(DcMotorEx.ZeroPowerBehavior.BRAKE);
+
+        //Code is looped inside this while loop
+        while (opModeIsActive()) {
+
+            //Slow Driving
+            if (gamepad1.x) {
+                telemetry.addData("Mode", "PRECISE");
+                telemetry.update();
+                driveValue = 0.4;
+            }
+
+            //Reversed Driving
+            if (gamepad1.b) {
+                telemetry.addData("Mode", "REVERSED");
+                telemetry.update();
+                driveValue = -1;
+            }
+
+            //Normal Driving
+            if (gamepad1.a) {
+                telemetry.addData("Mode", "Normal");
+                telemetry.update();
+                driveValue = 1;
+            }
+
+
+            //Drive Code Using RR
+            mecanumDrive.setDrivePower(
+                    new Pose2d(-gamepad1.left_stick_y * driveValue,
+                            -gamepad1.right_stick_x * driveValue,
+                            -gamepad1.left_stick_x * driveValue));
+            mecanumDrive.updatePoseEstimate();
+
+
+
+            //Arm Control
+            double x = -gamepad2.left_stick_y;
+            armMotor1.setPower(x/0.6);
+            armMotor2.setPower(-(x/0.6));
+
+
+            //Range is between [0.0 and 1.0] 0.5 being the center
+            //Fix position values with testing
+            //Claw Toggle
+            //Falling Edge Detector
+            if (!currentGamepad2.left_bumper && previousGamepad2.left_bumper) {
+                if (claw1.getPosition() == 0) {
+                    claw1.setPosition(0.5);
+                } else {
+                    claw1.setPosition(0);
+                }
+            }
+
+            if (!currentGamepad2.right_bumper && previousGamepad2.right_bumper) {
+                if (claw1.getPosition() == 0) {
+                    claw1.setPosition(0.5);
+                } else {
+                    claw1.setPosition(0);
+                }
+            }
+
+
+
+
+
+        }
+
+    }
+
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -21,8 +21,8 @@ public class DriveConstants {
     /*
      * These are motor constants that should be listed online for your motors.
      */
-    public static final double TICKS_PER_REV = 1;
-    public static final double MAX_RPM = 1;
+    public static final double TICKS_PER_REV = 1120;
+    public static final double MAX_RPM = 150;
 
     /*
      * Set RUN_USING_ENCODER to true to enable built-in hub velocity control using drive encoders.
@@ -44,9 +44,9 @@ public class DriveConstants {
      * angular distances although most angular parameters are wrapped in Math.toRadians() for
      * convenience. Make sure to exclude any gear ratio included in MOTOR_CONFIG from GEAR_RATIO.
      */
-    public static double WHEEL_RADIUS = 2; // in
+    public static double WHEEL_RADIUS = 1.8898; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 1; // in
+    public static double TRACK_WIDTH = 15; // in
 
     /*
      * These are the feedforward parameters used to model the drive motor behavior. If you are using
@@ -65,18 +65,18 @@ public class DriveConstants {
      * small and gradually increase them later after everything is working. All distance units are
      * inches.
      */
-    public static double MAX_VEL = 30;
-    public static double MAX_ACCEL = 30;
-    public static double MAX_ANG_VEL = Math.toRadians(60);
-    public static double MAX_ANG_ACCEL = Math.toRadians(60);
+    public static double MAX_VEL = 25.23217263620446;
+    public static double MAX_ACCEL = 25.23217263620446;
+    public static double MAX_ANG_VEL = Math.toRadians(80.31649999999999);
+    public static double MAX_ANG_ACCEL = Math.toRadians(80.31649999999999);
 
     /*
      * Adjust the orientations here to match your robot. See the FTC SDK documentation for details.
      */
     public static RevHubOrientationOnRobot.LogoFacingDirection LOGO_FACING_DIR =
-            RevHubOrientationOnRobot.LogoFacingDirection.UP;
+            RevHubOrientationOnRobot.LogoFacingDirection.LEFT;
     public static RevHubOrientationOnRobot.UsbFacingDirection USB_FACING_DIR =
-            RevHubOrientationOnRobot.UsbFacingDirection.FORWARD;
+            RevHubOrientationOnRobot.UsbFacingDirection.UP;
 
 
     public static double encoderTicksToInches(double ticks) {
